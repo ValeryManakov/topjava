53f05565fc2b8ff60534a731c4ed036b4601b6b5
ValeryManakov <valera.manakov.93@bk.ru>
2021-12-12 19:12:03 +0300

    11_06_HW10_fix_update_duplicate

--- src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
@@ -21,6 +21,7 @@ import ru.javawebinar.topjava.util.exception.IllegalRequestDataException;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
 import static ru.javawebinar.topjava.util.exception.ErrorType.*;
@@ -33,9 +34,7 @@ public class ExceptionInfoHandler {
     public static final String EXCEPTION_DUPLICATE_EMAIL = "exception.user.duplicateEmail";
     public static final String EXCEPTION_DUPLICATE_DATETIME = "exception.meal.duplicateDateTime";
 
-    private static final Map<String, String> CONSTRAINS_I18N_MAP = Map.of(
-            "users_unique_email_idx", EXCEPTION_DUPLICATE_EMAIL,
-            "meals_unique_user_datetime_idx", EXCEPTION_DUPLICATE_DATETIME);
+    private static final Map<String, String> CONSTRAINS_I18N_MAP = getMap();
 
     private final MessageSourceAccessor messageSourceAccessor;
 
@@ -92,5 +91,12 @@ public class ExceptionInfoHandler {
         Throwable rootCause = ValidationUtil.logAndGetRootCause(log, req, e, logStackTrace, errorType);
         return new ErrorInfo(req.getRequestURL(), errorType,
                 details.length != 0 ? details : new String[]{ValidationUtil.getMessage(rootCause)});
+    }
+
+    private static Map<String, String> getMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("users_unique_email_idx", EXCEPTION_DUPLICATE_EMAIL);
+        map.put("meals_unique_user_datetime_idx", EXCEPTION_DUPLICATE_DATETIME);
+        return map;
     }
 }
